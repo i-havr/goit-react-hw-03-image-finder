29305499381769b22e8fdd99262545f0ba46c748
i-havr <i.havrylov@gmail.com>
2022-12-29 18:03:52 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,29 +3,29 @@ import { createPortal } from 'react-dom';
 const modalRoot = document.querySelector('#modal-root');
 import { BackdropStyled, ModalStyled } from './Modal.styled';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
 
-  handleKeyDown = event => event.code === 'Escape' && this.props.onClose();
+//   handleKeyDown = event => event.code === 'Escape' && this.props.onClose();
 
-  handleBackdropClick = event =>
-    event.currentTarget === event.target && this.props.onClose();
+//   handleBackdropClick = event =>
+//     event.currentTarget === event.target && this.props.onClose();
 
-  render() {
-    return createPortal(
-      <BackdropStyled>
-        {/* <BackdropStyled onClick={}> */}
-        <ModalStyled>
-          <img src="" alt="" />
-        </ModalStyled>
-      </BackdropStyled>,
-      modalRoot
-    );
-  }
-}
+//   render() {
+//     return createPortal(
+
+//         <BackdropStyled onClick={onClick}>
+//         <ModalStyled>
+//           <img src="" alt="" />
+//         </ModalStyled>
+//       </BackdropStyled>,
+//       modalRoot
+//     );
+//   }
+// }
